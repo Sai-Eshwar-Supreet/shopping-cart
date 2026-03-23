002edc48e0fd42663e5d3a9b09a6002b0477db36
Sai-Eshwar-Supreet <saieshwarsupreet@gmail.com>
2026-03-23 06:40:17 +0000

    update routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,7 +2,6 @@ import { createBrowserRouter } from "react-router";
 import App from './App.jsx';
 import Home from './pages/Home.jsx';
 import Cart from './pages/Cart.jsx';
-import About from './pages/About.jsx';
 import Product from './pages/Product.jsx';
 
 export const routes = createBrowserRouter([
@@ -17,10 +16,6 @@ export const routes = createBrowserRouter([
       {
         path: 'cart',
         element: <Cart />
-      },
-      {
-        path: 'about',
-        element: <About />
       },
       {
         path: 'product/:id',
